51f235f64f79647521c6cbbce02c506009cdd750
Shahad shafi <shahadshafi497@gmail.com>
2021-10-03 23:31:13 +0500

    changed logo, form submission to email, removed job from navigation

--- mail-handler.php
+++ mail-handler.php
@@ -1,44 +1,48 @@
-<?php 
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
 
-if( isset($_POST['apply-button']) ){
-    // Send to this email address
-    $toEmailAddress = "info@the-solutionist.com";
-
-    // Get from email address from form post
-    $fromEmailAddress = $_POST['register_email']; 
-
-    // Get Full name from form post
-    $firstName = $_POST['register_first_name'];
-    $lastName = $_POST['register_last_name'];
+<?php
+    $filenameee =  $_FILES['resume_upload']['name'];
+    $fileName = $_FILES['resume_upload']['tmp_name']; 
+    $fname = $_POST['register_first_name'];
+    $lname = $_POST['register_last_name'];
+    $email = $_POST['register_email'];
     
-    $fullName = $firstName .' '. $lastName;
+    $message ="First Name = ". $fname ." ". "\r\n Last name = ". $lname . "\r\n  Email = " . $email; 
     
-    
-    // The email subject
-    $emailSubject = "Contact Us Form Submission at demo.com";
-    
-    // Copy email subject line 
-    $copyEmailSubject = "Copy of Contact Us Form Submission at demo.com";
-
-    // The email body string 
-    $emailBody = $fullName . " wrote the following:" . "\n\n" . $message;
-    
-    // The copy email body string
-    $copyEmailBody = "Here is a copy of your message " . $fullName . "\n\n" . $message;
-
-    // Email headers for both orignal and copy emails
-    $emailHeaders = "From:" . $fromEmailAddress;
-    $copyEmailHeaders = "From:" . $toEmailAddress;
-
-    // Send the orignal and copy emails
-    mail($toEmailAddress,$emailSubject,$emailBody,$emailHeaders);
-    mail($fromEmailAddress,$copyEmailSubject,$copyEmailBody,$copyEmailHeaders); 
-    
-    // Redirect User to thank you page
-    header('Location: https://the-solutionist.com/thankYou.html');
+    $subject ="application form";
+    $fromname ="The solutionist";
+    $fromemail = "kaamir4400@gmail.com";  //if u dont have an email create one on your cpanel
+    $mailto = 'info@the-solutionist.com';  //the email which u want to recv this email
+    $content = file_get_contents($fileName);
+    $content = chunk_split(base64_encode($content));
+    // a random hash will be necessary to send mixed content
+    $separator = md5(time());
+    // carriage return type (RFC)
+    $eol = "\r\n";
+    // main header (multipart mandatory)
+    $headers = "From: ".$fromname." <".$fromemail.">" . $eol;
+    $headers .= "MIME-Version: 1.0" . $eol;
+    $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+    $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
+    $headers .= "This is a MIME encoded message." . $eol;
+    // message
+    $body = "--" . $separator . $eol;
+    $body .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
+    $body .= "Content-Transfer-Encoding: 8bit" . $eol;
+    $body .= $message . $eol;
+    // attachment
+    $body .= "--" . $separator . $eol;
+    $body .= "Content-Type: application/octet-stream; name=\"" . $filenameee . "\"" . $eol;
+    $body .= "Content-Transfer-Encoding: base64" . $eol;
+    $body .= "Content-Disposition: attachment" . $eol;
+    $body .= $content . $eol;
+    $body .= "--" . $separator . "--";
+    //SEND Mail
+    if (mail($mailto, $subject, $body, $headers)) {
+     echo "<script>window.location.href='https://the-solutionist.com/thankYou.html';</script>";
+    exit;
+        
+        
+    } else {
+        echo "mail send ... ERROR!";
+        print_r( error_get_last() );
     }
-    
-?>
